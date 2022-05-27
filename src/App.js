6f42cdbc4cd47f7ed909b3d7a16b9e1661aed2bb
Justas <46055975+Justas94@users.noreply.github.com>
2022-05-27 17:22:34 +0100

    Promises

--- src/App.js
+++ src/App.js
@@ -5,25 +5,37 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      name: "Justas",
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello my name is {this.state.name}</p>
-          <button
-            onClick={() => {
-              this.setState({ name: "Andrius" });
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
